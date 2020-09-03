6bab1893a833b2a91c8700452454b31c8e274c5a
xavigu <xavigu91@gmail.com>
2020-09-03 14:02:29 +0200

    Add pizzas reducers, add index.ts where we are going to have all the reducers and import ngrx/store and reducers to products.module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,30 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { StoreModule } from "@ngrx/store";
+import { reducers } from "./store";
 
 // components
-import * as fromComponents from './components';
+import * as fromComponents from "./components";
 
 // containers
-import * as fromContainers from './containers';
+import * as fromContainers from "./containers";
 
 // services
-import * as fromServices from './services';
+import * as fromServices from "./services";
 
 // routes
 export const ROUTES: Routes = [
   {
-    path: '',
+    path: "",
     component: fromContainers.ProductsComponent,
   },
   {
-    path: ':id',
+    path: ":id",
     component: fromContainers.ProductItemComponent,
   },
   {
-    path: 'new',
+    path: "new",
     component: fromContainers.ProductItemComponent,
   },
 ];
@@ -35,6 +38,7 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
+    StoreModule.forFeature("products", reducers),
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
