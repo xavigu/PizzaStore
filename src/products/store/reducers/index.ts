60b66f60603def0ee5a2b80736df4facd9157edc
xavigu <xavigu91@gmail.com>
2020-09-03 18:23:23 +0200

    Create selectors in the global reducer to get data from the arrow functions of the pizzas reducer and use getAllPizzas selector data into pizzas$ observable of products component.

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,8 @@
-import { ActionReducerMap } from "@ngrx/store";
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+} from "@ngrx/store";
 import * as fromPizzas from "./pizzas.reducer";
 
 export interface ProductState {
@@ -8,3 +12,23 @@ export interface ProductState {
 export const reducers: ActionReducerMap<ProductState> = {
   pizzas: fromPizzas.reducer,
 };
+
+// selector of the object "products" inside of the whole store object
+export const getProductsState = createFeatureSelector<ProductState>("products");
+
+// selector of the object "pizzas" inside of the object "products"
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductState) => state.pizzas
+);
+
+// selector to get data of each property of object "pizzas"
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
