7586b4abeecc3f624e65dba62d233e85a801c96a
xavigu <xavigu91@gmail.com>
2020-09-16 17:06:05 +0200

    Create toppingsGuard to loadToppings fromStore when you are in "pizzaId" and "new" routes

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,7 +1,9 @@
 import { PizzasGuard } from './pizzas.guard';
 import { PizzasExistsGuard } from './pizza-exists.guard';
+import { ToppingsGuard } from './toppings.guard';
 
-export const guards: any[] = [PizzasGuard, PizzasExistsGuard];
+export const guards: any[] = [PizzasGuard, PizzasExistsGuard, ToppingsGuard];
 
 export * from './pizzas.guard';
 export * from './pizza-exists.guard';
+export * from './toppings.guard';
